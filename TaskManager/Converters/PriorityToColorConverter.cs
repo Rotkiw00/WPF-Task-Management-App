665d889669a5dfb8d43e3e10168356f6c8057c58
Wiktor Kalaga <wiktor.kalaga@comarch.com>
2025-10-30 00:14:15 +0100

    Enhanced UI with new styles and converters

---
+++ TaskManager/Converters/PriorityToColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using TaskManager.Core.Enums;
+
+namespace TaskManager.Converters;
+
+public class PriorityToColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not Priority priority)
+            return Brushes.Gray;
+
+        return priority switch
+        {
+            Priority.Low => Brushes.Green,
+            Priority.Medium => Brushes.Orange,
+            Priority.High => Brushes.OrangeRed,
+            Priority.Critical => Brushes.Red,
+            _ => Brushes.Gray
+        };
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ TaskManager/Converters/StatusToColorConverter.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using TaskManager.Core.Enums;
+
+namespace TaskManager.Converters;
+
+public class StatusToColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not Status status)
+            return Brushes.Gray;
+
+        return status switch
+        {
+            Status.Draft => Brushes.LightGray,
+            Status.Assigned => Brushes.Orange,
+            Status.InProgress => Brushes.Blue,
+            Status.UnderReview => Brushes.Purple,
+            Status.Completed => Brushes.Green,
+            Status.Rejected => Brushes.Red,
+            _ => Brushes.Gray
+        };
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
